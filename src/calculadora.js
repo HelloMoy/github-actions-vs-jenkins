eece76399e3e81f6be24fe88d4f4e8f82a3c89d1
hellomoy <bruma.hello.moy@gmail.com>
2026-06-05 15:53:55 -0600

    Arreglar el bug: sumar vuelve a sumar correctamente

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,7 +1,7 @@
 // Una función muy simple: suma dos números.
 // Esto es "el código de nuestra aplicación".
 function sumar(a, b) {
-  return a - b; // 🐛 BUG A PROPÓSITO: debería ser +, no -
+  return a + b;
 }
 
 // La exportamos para que las pruebas puedan usarla.
